6a117b5af8f5ee39968096bbc3d05a1b1848885d
Sebastian Zillien <szillien@hs-worms.de>
2023-07-17 10:53:54 +0200

    add experimental robust mode to decode_all

--- scripts/offline_Mode/decode_all.py
+++ scripts/offline_Mode/decode_all.py
@@ -2,14 +2,8 @@ import glob
 import re
 import subprocess
 
-short_1 = open("short.msg", "r").read()
-short_2 = open("short2.msg", "r").read()
-short_3 = open("short3.msg", "r").read()
-fox = open("fox.msg", "r").read()
-long = open("long.msg", "r").read()
-
 for file_path in glob.glob("*.out"):
-    re_matches = re.search("(office|remote|home)_([^_]+)_.*robust_(ext|basic)?_?(1|2)byte\.out", file_path)
+    re_matches = re.search("(office|remote|home)_([^_]+)_(non_robust|robust)_(ext|basic)?_?(1|2)byte\.out", file_path)
     print(file_path)
 
     if re_matches.groups()[0] == "remote":
@@ -19,18 +13,21 @@ for file_path in glob.glob("*.out"):
     elif re_matches.groups()[0] == "home":
         recording_path = "home_legit.pcap"
 
-    if re_matches.groups()[2] == "ext":
+    if re_matches.groups()[3] == "ext":
         arguments = ["tsp", "python", "offline_decoder_v2.py",
                      "-l", file_path,
                      "-p", recording_path,
-                     "-nc", re_matches.groups()[3],
+                     "-nc", re_matches.groups()[4],
                      "-tc", "0",
                      "-e"]
     else:
         arguments = ["tsp", "python", "offline_decoder_v2.py",
                      "-l", file_path,
                      "-p", recording_path,
-                     "-nc", re_matches.groups()[3],
+                     "-nc", re_matches.groups()[4],
                      "-tc", "0"]
+
+    if re_matches.groups()[2] == "robust":
+        arguments.append("-r")
 
     subprocess.run(arguments)
